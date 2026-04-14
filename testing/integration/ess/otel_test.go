88ffdb421e32921ff7cc362e6fb30fd76b316334
Khushi Jain <khushi.jain@elastic.co>
2026-04-14 15:56:11 +0530

    add ssl tests using beats image

--- testing/integration/ess/otel_test.go
+++ testing/integration/ess/otel_test.go
@@ -2,8 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
-//go:build integration
-
 package ess
 
 import (
@@ -18,6 +16,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -33,7 +32,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/testcontainers/testcontainers-go/modules/kafka"
+	"github.com/testcontainers/testcontainers-go/modules/compose"
 
 	"github.com/elastic/elastic-agent-libs/mapstr"
 	"github.com/elastic/elastic-agent-libs/testing/estools"
@@ -2832,28 +2831,64 @@ func (w *ZapWriter) Write(p []byte) (n int, err error) {
 }
 
 func TestSystemMetricsWithKafkaOutput(t *testing.T) {
-	define.Require(t, define.Requirements{
-		Group: integration.Default,
-		Local: true,
-		OS: []define.OS{
-			{Type: define.Linux},
-			{Type: define.Darwin},
-		},
-		Stack: &define.Stack{},
-	})
+	// define.Require(t, define.Requirements{
+	// 	Group: integration.Default,
+	// 	Local: true,
+	// 	OS: []define.OS{
+	// 		{Type: define.Linux},
+	// 		{Type: define.Darwin},
+	// 	},
+	// 	Stack: &define.Stack{},
+	// })
 
-	k, err := kafka.Run(t.Context(),
-		"confluentinc/confluent-local:7.5.0",
-		kafka.WithClusterID("test-cluster"),
-	)
+	_, currentFile, _, ok := runtime.Caller(0)
+	require.True(t, ok, "failed to get current file path")
+	kafkaPath := filepath.Join(filepath.Dir(currentFile), "..", "..", "..", "beats", "testing", "environments", "docker", "kafka")
+	fmt.Println(kafkaPath)
 
+	composeContent := fmt.Sprintf(`
+services:
+  kafka:
+    build: %s
+    ports:
+      - 9092:9092
+      - 9093:9093
+      - 9094:9094
+      - 2181:2181
+    environment:
+      - ADVERTISED_HOST=kafka
+`, kafkaPath)
+
+	stack, err := compose.NewDockerComposeWith(compose.WithStackReaders(strings.NewReader(composeContent)))
 	require.NoError(t, err)
+
 	t.Cleanup(func() {
-		_ = k.Terminate(context.Background())
+		_ = stack.Down(
+			context.Background(),
+			compose.RemoveOrphans(true),
+			compose.RemoveVolumes(true),
+			compose.RemoveImagesLocal,
+		)
 	})
 
-	brokers, err := k.Brokers(t.Context())
+	err = stack.
+		Up(t.Context(), compose.Wait(true))
 	require.NoError(t, err)
+
+	// k, err := kafka.Run(t.Context(),
+	// 	"confluentinc/confluent-local:7.5.0",
+	// 	kafka.WithClusterID("test-cluster"),
+	// )
+
+	// require.NoError(t, err)
+	// t.Cleanup(func() {
+	// 	_ = k.Terminate(context.Background())
+	// })
+
+	// brokers, err := k.Brokers(t.Context())
+	// require.NoError(t, err)
+
+	var brokers = "kafka:9093"
 
 	kafkaDocs := make(map[string]mapstr.M, 0)
 
@@ -2869,6 +2904,7 @@ func TestSystemMetricsWithKafkaOutput(t *testing.T) {
 		type otelConfigOptions struct {
 			RuntimeExperimental string
 			Broker              string
+			CaCert              string
 		}
 
 		fixture, err := define.NewFixtureFromLocalBuild(t, define.Version())
@@ -2897,6 +2933,12 @@ outputs:
     required_acks: 1
     broker_timeout: 30s
     queue.mem.flush.timeout: 1s
+    ssl.certificate_authorities: 
+    - {{.CaCert}}
+    username: beats
+    password: KafkaTest
+    protocol: https
+    sasl.mechanism: SCRAM-SHA-256
 agent.monitoring:
   metrics: false
   logs: false
@@ -2908,7 +2950,8 @@ agent.monitoring:
 		template.Must(template.New("config").Parse(configTemplate)).Execute(&configBuffer,
 			otelConfigOptions{
 				RuntimeExperimental: tt.runtimeExperimental,
-				Broker:              brokers[0],
+				Broker:              brokers,
+				CaCert:              filepath.Join(kafkaPath, "certs", "ca-cert"),
 			})
 
 		ctx, cancel := testcontext.WithDeadline(t, context.Background(), time.Now().Add(5*time.Minute))
@@ -2944,7 +2987,7 @@ agent.monitoring:
 			return true
 		}, 30*time.Second, 1*time.Second)
 
-		consumer, err := sarama.NewConsumer([]string{brokers[0]}, sarama.NewConfig())
+		consumer, err := sarama.NewConsumer([]string{"0.0.0.0:9092"}, sarama.NewConfig())
 		require.NoError(t, err)
 
 		partitionConsumer, err := consumer.ConsumePartition("metrics-e2e-"+tt.runtimeExperimental, 0, sarama.OffsetNewest)
